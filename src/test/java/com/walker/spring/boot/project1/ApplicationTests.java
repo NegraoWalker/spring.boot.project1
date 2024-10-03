2bdd34d2a6ea83aaabb3e47434e267a12e20b66c
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-10-03 07:45:14 -0300

    Repositório criado

---
+++ src/test/java/com/walker/spring/boot/project1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.spring.boot.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
